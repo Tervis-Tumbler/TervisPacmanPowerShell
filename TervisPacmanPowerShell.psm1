1c777404e2c45f55d811997e50b80d90daebe711
ChrisMagnuson <hourglasssand@gmail.com>
2017-11-02 16:39:25 -0400

    Initial commit

---
+++ TervisPacmanPowerShell.psm1
@@ -0,0 +1,47 @@
+﻿$TervisPackageGroups = [PSCustomObject][Ordered] @{
+    Name = "ArchRouter"
+    PackageName = @"
+bmon
+fish
+nftables
+openssh
+pacman
+parted
+sudo
+tmux
+"@ -split "`r`n"
+}
+
+function Get-TervisPacmanPackageGroup {
+    param (
+        $Name
+    )
+    $TervisPackageGroups | where Name -EQ $Name
+}
+
+
+function New-PacmanPackageInstallCommand {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]$PackageName
+    )
+    begin { $PackageNames = @() }
+    process {
+        $PackageNames += $PackageName
+    }
+    end {
+        $Arguements = $PackageNames -join " "
+        "pacman -Syu $Arguements"
+    }
+}
+
+function Install-PacmanTervisPackageGroup {
+    param (
+        [Parameter(Mandatory)]$TervisPackageGroupName,
+        [Parameter(Mandatory,ParameterSetName="SSHSession")]$SSHSession
+    )
+    process {
+        $TervisPackageGroup = Get-TervisPacmanPackageGroup -Name $TervisPackageGroupName
+        $Command = $TervisPackageGroup.PackageName | New-PacmanPackageInstallCommand
+        Invoke-SSHCommand -Command $Command -SSHSession $SSHSession
+    }
+}
